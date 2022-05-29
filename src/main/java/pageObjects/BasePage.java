082a23844f1c2bf2d92e53b925cad1adaef567b7
vishnu mohan <vishnu.mohan@redblacksoftware.com>
2022-05-29 21:52:46 +0530

    committing updates

--- src/main/java/pageObjects/BasePage.java
+++ src/main/java/pageObjects/BasePage.java
@@ -19,7 +19,7 @@ public class BasePage {
 
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
-		wait = new WebDriverWait(driver, 10);
+		wait = new WebDriverWait(driver, 15);
 
 	}
 
--- src/main/java/pageObjects/UserHomePage.java
+++ src/main/java/pageObjects/UserHomePage.java
@@ -13,6 +13,7 @@ public class UserHomePage extends BasePage {
 	private By inputWhatsOnMind = By.cssSelector("div.m9osqain.a5q79mjw.gy2v8mqq.jm1wdb64.k4urcfbm.qv66sw1b");
 	private By inputWritePost = By.cssSelector("p.i1ao9s8h.hcukyx3x.oygrvhab.cxmmr5t8.kvgmc6g5");
 	private By buttonPost = By.cssSelector("div.k4urcfbm.discj3wi.dati1w0a.hv4rvrfc.i1fnvgqd.j83agx80.rq0escxv.bp9cbjyn div.bp9cbjyn.j83agx80.taijpn5t.c4xchbtz");
+	private By dialogCreatePost = By.cssSelector("div.j83agx80.cbu4d94t.f0kvp8a6.mfofr4af.l9j0dhe7.oh7imozk.ij1vhnid.smbo3krw");
 	
 	private By getPostLocator(String post) {
 		
@@ -38,7 +39,7 @@ public class UserHomePage extends BasePage {
 			waitForElement(inputWritePost);
 			enterText(inputWritePost,post);
 			click(buttonPost);
-			waitForElementToDisappear(buttonPost);
+			waitForElementToDisappear(dialogCreatePost);
 		} catch (Exception e) {
 			return "Posting status failed -" + e;
 		}
